9cf52ab1ec4b0392f91d00c21c50cb9c175aac31
HromovAndrey <andrey.gromov.36912@gmail.com>
2024-03-24 19:39:53 +0300

    Створення класу BankAccount

--- OOP_mod_11_part_2.py
+++ OOP_mod_11_part_2.py
@@ -1,19 +1,38 @@
-#Завдання 4
-# Створіть клас Car з атрибутами brand (марка
-#автомобіля), model (модель) та year (рік випуску).
-#Додайте метод start_engine, який виведе повідомлення
-#про те, що двигун запущено.
-class Car:
-    def __init__(self, brand, model, year):
-        self.brand = brand
-        self.model = model
-        self.year = year
+#Завдання 5
+#Створіть клас BankAccount з атрибутами balance
+#та owner, а також методами deposit та withdraw для
+#здійснення операцій з балансом. Реалізуйте перевірку
+#на те, що баланс не може стати від'ємним.
+class BankAccount:
+    def __init__(self, owner, initial_balance=0):
+        self.owner = owner
+        self.balance = initial_balance
 
-    def start_engine(self):
-        print("Двигун запущено")
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Поповнення на {amount} грн успішно здійснено.")
+        else:
+            print("Помилка: Сума поповнення повинна бути більше нуля.")
 
-car1 = Car("Toyota", "Camry", 2022)
-car1.start_engine()
+    def withdraw(self, amount):
+        if amount > 0:
+            if self.balance >= amount:
+                self.balance -= amount
+                print(f"Зняття {amount} грн успішно здійснено.")
+            else:
+                print("Помилка: Недостатньо коштів на рахунку.")
+        else:
+            print("Помилка: Сума зняття повинна бути більше нуля.")
 
-car2 = Car("Honda", "Civic", 2020)
-car2.start_engine()
+account = BankAccount("John Doe", 1000)
+print(f"Баланс рахунку: {account.balance} грн")
+
+account.deposit(500)
+print(f"Після поповнення баланс рахунку: {account.balance} грн")
+
+account.withdraw(200)
+print(f"Після зняття баланс рахунку: {account.balance} грн")
+
+account.withdraw(2000)
+print(f"Після спроби зняття недостатньої суми баланс рахунку: {account.balance} грн")

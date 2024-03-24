024f58a08ddcc3a6ae71757d33682695c20c6a6b
HromovAndrey <andrey.gromov.36912@gmail.com>
2024-03-24 19:26:26 +0300

    Примінення return в методах

--- OOP_mod_11_part_2.py
+++ OOP_mod_11_part_2.py
@@ -1,26 +1,15 @@
+#Завдання 2
+#Створіть клас Circle з атрибутом radius та методом
+#area, який поверне площу кола з вказаним радіусом.
 
-class Dogs:
-    def __int__(self, name, age, bread):
-        self.name = name
-        self.age = age
-        self.bread = bread
-    pass
-#створення  екземпляру класу
-my_dogs = Dogs("Бобік", 3, "Лабродор" )
-print(my_dogs.name)
-print(my_dogs.age)
+import math
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
 
-#Завдання 1
-#Створіть клас Student з атрибутами name та age.
-#Додайте метод print_info, який виведе інформацію про
-#студента у на вигляді "Ім'я: {name}, Вік: {age}".
+    def area(self):
+        return math.pi * (self.radius ** 2)
 
-class Student:
-    def __init__(self, name,age):
-        self.name = name
-        self.age = age
-
-    def  print_info(self):
-        print(f"Імя студента та {self.name}його вік{self.age}")
-student1 = Student("Іван", 20)
-student1.print_info()
+radius = 5
+circle = Circle(radius)
+print("Площа кола з радіусом", radius, "дорівнює", circle.area())

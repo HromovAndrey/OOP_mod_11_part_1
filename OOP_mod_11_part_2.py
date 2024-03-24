f8fe79810b24873b43ba3adae847955d556d4480
HromovAndrey <andrey.gromov.36912@gmail.com>
2024-03-24 19:14:46 +0300

    клас Student

--- OOP_mod_11_part_2.py
+++ OOP_mod_11_part_2.py
@@ -9,3 +9,18 @@ class Dogs:
 my_dogs = Dogs("Бобік", 3, "Лабродор" )
 print(my_dogs.name)
 print(my_dogs.age)
+
+#Завдання 1
+#Створіть клас Student з атрибутами name та age.
+#Додайте метод print_info, який виведе інформацію про
+#студента у на вигляді "Ім'я: {name}, Вік: {age}".
+
+class Student:
+    def __init__(self, name,age):
+        self.name = name
+        self.age = age
+
+    def  print_info(self):
+        print(f"Імя студента та {self.name}його вік{self.age}")
+student1 = Student("Іван", 20)
+student1.print_info()

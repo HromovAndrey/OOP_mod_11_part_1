d1794cdc99fe51005834e7124c0ac95e06d7263f
HromovAndrey <andrey.gromov.36912@gmail.com>
2024-03-24 19:33:42 +0300

    Примінення return в методах

--- OOP_mod_11_part_2.py
+++ OOP_mod_11_part_2.py
@@ -1,15 +1,20 @@
-#Завдання 2
-#Створіть клас Circle з атрибутом radius та методом
-#area, який поверне площу кола з вказаним радіусом.
+#Завдання 3
+#Створіть клас Book з атрибутами title (назва
+#книги), author (автор) та genre (жанр). Додайте метод
+#display_info, який виведе інформацію про книгу у
+# "Назва: {title}, Автор: {author}, Жанр: {genre}".
+class Book:
+    def __init__(self, title, author, genre):
+        self.title = title
+        self.author = author
+        self.genre = genre
 
-import math
-class Circle:
-    def __init__(self, radius):
-        self.radius = radius
+    def display_info(self):
+        print(f"Назва: {self.title}, Автор: {self.author}, Жанр: {self.genre}")
 
-    def area(self):
-        return math.pi * (self.radius ** 2)
+book1 = Book("Війна і мир", "Лев Толстой", "Роман")
+book1.display_info()
 
-radius = 5
-circle = Circle(radius)
-print("Площа кола з радіусом", radius, "дорівнює", circle.area())
+book2 = Book("1984", "Джордж Орвелл", "Фантастика")
+book2.display_info()
+
